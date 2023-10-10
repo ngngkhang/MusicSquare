264dbecf9266fac6266d60b33c9f4662c0209cd0
ngngkhang <khangbeo306@gmail.com>
2023-10-10 16:03:59 +0700

    vercel

--- musicSquare/src/App.jsx
+++ musicSquare/src/App.jsx
@@ -1,22 +1,25 @@
-
-import './App.css'
-import Details from './components/Details/Details'
-import SliderContainer from './components/SliderContainer/SliderContainer'
-import Buttons from './components/Buttons/Buttons'
+import "./App.css";
+import Details from "./components/Details/Details";
+import SliderContainer from "./components/SliderContainer/SliderContainer";
+import Buttons from "./components/Buttons/Buttons";
 
 function App() {
-
   return (
     <>
       <div className="player">
         <div className="wrapper">
-          <Details/>
-          <SliderContainer/>
-          <Buttons/>
+          <Details />
+          <SliderContainer />
+          <Buttons />
+          <div className="list">
+            {" "}
+            <i className="fa-solid fa-chevron-down fa-2xl" />
+            Lyrics
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
